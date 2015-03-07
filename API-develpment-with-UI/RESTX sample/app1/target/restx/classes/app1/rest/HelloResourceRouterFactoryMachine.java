a2dd4a968bb63a43a431e165bf67690cd2384ae7
justin <justin4mail@gmail.com>
2015-03-07 16:01:30 +0530

    RestX Sample

---
+++ API-develpment-with-UI/RESTX sample/app1/target/restx/classes/app1/rest/HelloResourceRouterFactoryMachine.java
@@ -0,0 +1,46 @@
+package app1.rest;
+
+import com.google.common.collect.ImmutableSet;
+import restx.factory.*;
+import app1.rest.HelloResourceRouter;
+
+@Machine
+public class HelloResourceRouterFactoryMachine extends SingleNameFactoryMachine<HelloResourceRouter> {
+    public static final Name<HelloResourceRouter> NAME = Name.of(HelloResourceRouter.class, "HelloResourceRouter");
+
+    public HelloResourceRouterFactoryMachine() {
+        super(0, new StdMachineEngine<HelloResourceRouter>(NAME, 0, BoundlessComponentBox.FACTORY) {
+private final Factory.Query<app1.rest.HelloResource> resource = Factory.Query.byClass(app1.rest.HelloResource.class).mandatory();
+private final Factory.Query<restx.entity.EntityRequestBodyReaderRegistry> readerRegistry = Factory.Query.byClass(restx.entity.EntityRequestBodyReaderRegistry.class).mandatory();
+private final Factory.Query<restx.entity.EntityResponseWriterRegistry> writerRegistry = Factory.Query.byClass(restx.entity.EntityResponseWriterRegistry.class).mandatory();
+private final Factory.Query<restx.converters.MainStringConverter> converter = Factory.Query.byClass(restx.converters.MainStringConverter.class).mandatory();
+private final Factory.Query<javax.validation.Validator> validator = Factory.Query.byClass(javax.validation.Validator.class).mandatory();
+private final Factory.Query<restx.security.RestxSecurityManager> securityManager = Factory.Query.byClass(restx.security.RestxSecurityManager.class).mandatory();
+
+            @Override
+            public BillOfMaterials getBillOfMaterial() {
+                return new BillOfMaterials(ImmutableSet.<Factory.Query<?>>of(
+resource,
+readerRegistry,
+writerRegistry,
+converter,
+validator,
+securityManager
+                ));
+            }
+
+            @Override
+            protected HelloResourceRouter doNewComponent(SatisfiedBOM satisfiedBOM) {
+                return new HelloResourceRouter(
+satisfiedBOM.getOne(resource).get().getComponent(),
+satisfiedBOM.getOne(readerRegistry).get().getComponent(),
+satisfiedBOM.getOne(writerRegistry).get().getComponent(),
+satisfiedBOM.getOne(converter).get().getComponent(),
+satisfiedBOM.getOne(validator).get().getComponent(),
+satisfiedBOM.getOne(securityManager).get().getComponent()
+                );
+            }
+        });
+    }
+
+}
